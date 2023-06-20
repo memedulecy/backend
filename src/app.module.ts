bfdea342cec59c8a5efe35376d18eef87bb1da18
DOHEE <dohee212@idstrust.com>
2023-06-20 13:55:08 +0900

    [FEAT] exception filter 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
+// library
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+// common
+import { HttpExceptionFilter } from 'COMMON/filters/httpException.filter';
+// controller
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// module
 import { EnvModule } from './env/env.module';
+// service
+import { AppService } from './app.service';
 
 @Module({
     imports: [EnvModule],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [AppService, { provide: APP_FILTER, useClass: HttpExceptionFilter }],
 })
 export class AppModule {}
---
+++ src/common/filters/httpException.filter.ts
@@ -0,0 +1,59 @@
+// library
+import { Response } from 'express';
+import { ExceptionFilter, ArgumentsHost, Catch, HttpStatus, NotFoundException } from '@nestjs/common';
+// exception
+import { ErrMsg } from 'EXCEPTION/errMsg';
+import { ErrCode } from 'EXCEPTION/errCode';
+import { IntegrateException } from 'EXCEPTION/integrateException';
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+    catch(err, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const res = ctx.getResponse();
+        console.error(err);
+
+        if (err instanceof IntegrateException) {
+            this.integrateErrRes(res, err);
+        } else if (err instanceof NotFoundException) {
+            this.pathErrRes(res, err);
+        } else if (err.response) {
+            if (Array.isArray(err.response.message) && err.response.message.length) {
+                this.validationErrRes(res, err);
+            } else {
+                this.nestErrRes(res, err);
+            }
+        } else if (err instanceof Error) {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ errMsg: err.message, errCode: ErrCode.INTERNAL_SERVER_ERROR });
+        } else {
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                errMsg: ErrMsg.INTERNAL_SERVER_ERROR,
+                errCode: ErrCode.INTERNAL_SERVER_ERROR,
+            });
+        }
+    }
+
+    private integrateErrRes(res: Response, err: IntegrateException): void {
+        const { statusCode, errCode, errMsg } = err.res;
+        res.status(statusCode).json({ errMsg, errCode });
+    }
+
+    private pathErrRes(res: Response, err: NotFoundException): void {
+        res.status(err.getStatus()).json({ errMsg: ErrMsg.INVALID_PATH, errCode: ErrCode.INVALID_PATH });
+    }
+
+    private validationErrRes(res: Response, err: any): void {
+        const REPRESENT_IDX = 0;
+        const TYPE_IDX = 0;
+        const represent = err.response.message[REPRESENT_IDX].split(' ')[TYPE_IDX].toUpperCase();
+
+        res.status(err.getStatus()).json({
+            errDetail: err.response.message,
+            errMsg: `ERROR_TYPE_${represent}`,
+            errCode: ErrCode.BAD_REQUEST,
+        });
+    }
+
+    private nestErrRes(res: Response, err: any): void {
+        res.status(err.getStatus()).json({ errMsg: err.response.message, errCode: ErrCode.BAD_REQUEST });
+    }
+}
