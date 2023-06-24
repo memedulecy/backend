41fe79dc6092909849bbf8c9c1bdbad5fdc96c46
DOHEE <dohee212@naver.com>
2023-06-25 01:36:08 +0900

    [FEAT / SOCKET] 밈 생성 시 들을 리스너 추가 및 socketMap 외부로 분리

---
+++ src/common/const/socketMap.const.ts
@@ -0,0 +1,3 @@
+import { GpsData } from 'SRC/event/datatypes/interface/gps.interface';
+
+export const socketMap: Map<string, GpsData> = new Map();
--- src/event/events.gateway.ts
+++ src/event/events.gateway.ts
@@ -2,15 +2,14 @@ import { Logger } from '@nestjs/common';
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { EventType } from './datatypes/type/event.type';
-import { ObjectId } from 'mongodb';
 import { GpsBody, GpsData } from './datatypes/interface/gps.interface';
 import { MemeService } from 'MEME/meme.service';
 import getDistance from 'gps-distance';
+import { socketMap } from 'COMMON/const/socketMap.const';
 
 @WebSocketGateway(3000, { transports: ['websocket'], cors: true })
 export class EventsGateway {
     private logger = new Logger('Gateway');
-    private socketMap: Map<string, GpsData> = new Map();
     @WebSocketServer()
     server: Server;
 
@@ -21,25 +20,30 @@ export class EventsGateway {
     }
 
     handleDisconnect(@ConnectedSocket() socket: Socket) {
-        this.socketMap.delete(socket.id);
+        socketMap.delete(socket.id);
         this.logger.log(`💛 ${socket.id} 소켓 연결 해제 💛`);
     }
 
     @SubscribeMessage(EventType.SEND_GPS)
-    async handleMessage(@ConnectedSocket() socket: Socket, @MessageBody() gps: GpsBody) {
+    async handleGPSMessage(@ConnectedSocket() socket: Socket, @MessageBody() gps: GpsBody) {
         const data: GpsData = { location: [gps.long, gps.lat] };
         if (gps.token) data.userId = '649673d0a81c659607d29cd1';
-        this.socketMap.set(socket.id, data);
+        socketMap.set(socket.id, data);
         await this.resendMemes();
     }
 
-    public resendMemes = async () => {
+    @SubscribeMessage(EventType.CREATE_MEME)
+    async handleMemeMessage() {
+        await this.resendMemes();
+    }
+
+    private resendMemes = async () => {
         const VALUE_IDX = 1;
         await Promise.all(
-            Array.from(this.socketMap).map(async targetData => {
+            Array.from(socketMap).map(async targetData => {
                 const socketId = targetData[0];
                 const [long1, lat1] = targetData[VALUE_IDX].location;
-                const userIds = Array.from(this.socketMap)
+                const userIds = Array.from(socketMap)
                     .filter(socketData => {
                         if (!socketData[VALUE_IDX].userId || targetData[VALUE_IDX].userId === socketData[VALUE_IDX].userId) return false;
                         const [long2, lat2] = socketData[VALUE_IDX].location;
