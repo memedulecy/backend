e0aa6e3c95769e1ffc311c520529476719df0ffc
DOHEE <dohee212@naver.com>
2023-06-24 11:49:22 +0900

    [FEAT / FILE] s3 업로드 기능 추가

--- src/env/dataTypes/types/env.type.ts
+++ src/env/dataTypes/types/env.type.ts
@@ -2,7 +2,11 @@ export const Env = {
     PORT: 'PORT',
     DB_HOST: 'DB_HOST',
     DB_PORT: 'DB_PORT',
-    DB_DATABASE: 'DB_DATABASE'
+    DB_DATABASE: 'DB_DATABASE',
+    AWS_BUCKET_REGION: 'AWS_BUCKET_REGION',
+    AWS_BUCKET_NAME: 'AWS_BUCKET_NAME',
+    AWS_ACCESS_KEY_ID: 'AWS_ACCESS_KEY_ID',
+    AWS_SECRET_ACCESS_KEY: 'AWS_SECRET_ACCESS_KEY',
 } as const;
 
-export type EnvKey = keyof typeof Env;
+export type Env = keyof typeof Env;
