d12c7c35a6d27685cb840047ca3f6c0e62b83be2
DOHEE <dohee212@naver.com>
2023-06-24 14:04:15 +0900

    [FEAT / MEME] 밈 상세 조회 기능 추가

--- src/meme/meme.controller.ts
+++ src/meme/meme.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, HttpStatus, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { MemeService } from './meme.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { IntegrateException } from 'EXCEPTION/integrateException';
 import { ErrCode } from 'EXCEPTION/errCode';
 import { ErrMsg } from 'EXCEPTION/errMsg';
+import { MemeModel } from './entity/meme.model';
 
 @Controller('memes')
 export class MemeController {
@@ -11,13 +12,10 @@ export class MemeController {
 
     @Post('')
     @UseInterceptors(FileInterceptor('img'))
-    async create(@UploadedFile() img: Express.MulterS3.File, @Body() body: { message: string }) {
+    async create(@UploadedFile() img: Express.MulterS3.File, @Body() body: { message: string }): Promise<MemeModel> {
         const imgUrl = this.uploadImg(img);
         const message = body.message;
         const creator = 'DOHEE';
-        console.log('imgUrl', imgUrl);
-        console.log('message', message);
-        console.log('creator', creator);
 
         return this.memeService.create(imgUrl, message, creator);
     }
@@ -26,4 +24,9 @@ export class MemeController {
         if (!img) throw new IntegrateException(ErrCode.BAD_REQUEST, ErrMsg.BAD_REQUEST, HttpStatus.BAD_REQUEST);
         return img.location;
     };
+
+    @Get('/:memeId')
+    async findDetail(@Param('memeId') memeId: string): Promise<MemeModel> {
+        return await this.memeService.findDetail(memeId);
+    }
 }
--- src/meme/meme.service.ts
+++ src/meme/meme.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { MemeRepository } from './repository/meme.repository';
 import { MemeModel } from './entity/meme.model';
+import { IntegrateException } from 'EXCEPTION/integrateException';
+import { ErrCode } from 'EXCEPTION/errCode';
+import { ErrMsg } from 'EXCEPTION/errMsg';
 
 @Injectable()
 export class MemeService {
@@ -9,5 +12,12 @@ export class MemeService {
     public create = async (imgUrl: string, message: string, creator: string): Promise<MemeModel> => {
         const newMeme: Partial<MemeModel> = { imgUrl, message, creator, updater: creator };
         return await this.memeRepository.create(newMeme);
+    };
+
+    public findDetail = async (memeId: string): Promise<MemeModel> => {
+        const meme = await this.memeRepository.findOneById(memeId);
+        if (!meme) throw new IntegrateException(ErrCode.NOT_FOUND, ErrMsg.NOT_FOUND, HttpStatus.NOT_FOUND);
+
+        return meme;
     };
 }
