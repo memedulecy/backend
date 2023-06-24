a7f1e38448da11df4ac650c38f872bca0e2d0e3d
DOHEE <dohee212@naver.com>
2023-06-25 00:39:13 +0900

    [FEAT / SOCKET] 위치 변경 시 밈 목록 푸쉬

--- src/meme/repository/meme.repository.ts
+++ src/meme/repository/meme.repository.ts
@@ -2,7 +2,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { LocationModel } from 'MEME/entity/location.model';
 import { MemeModel } from 'MEME/entity/meme.model';
 import { ObjectId } from 'mongodb';
-import { MongoRepository } from 'typeorm';
+import { FindOptions, MongoRepository } from 'typeorm';
 
 export class MemeRepository {
     constructor(@InjectRepository(MemeModel) private readonly memeRepository: MongoRepository<MemeModel>) {}
@@ -15,5 +15,9 @@ export class MemeRepository {
 
     public findOneById = async (memeId: string): Promise<MemeModel> => {
         return await this.memeRepository.findOne({ where: { _id: new ObjectId(memeId) } });
+    };
+
+    public findByFilter = async (filter: FindOptions<MemeModel>) => {
+        return await this.memeRepository.find({ where: filter, order: { createdTs: 'DESC' } });
     };
 }
