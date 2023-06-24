d8e132e5d3a7d4b4e9aadbabbac02fe8242ff4cc
DOHEE <dohee212@naver.com>
2023-06-25 01:36:08 +0900

    [FEAT / SOCKET] 밈 생성 시 들을 리스너 추가 및 socketMap 외부로 분리

--- src/event/datatypes/type/event.type.ts
+++ src/event/datatypes/type/event.type.ts
@@ -1,5 +1,6 @@
 export const EventType = {
     SEND_GPS: 'sendGPS',
     SEND_MEMES: 'sendMemes',
+    CREATE_MEME: 'createMeme',
 } as const;
 export type EventType = (typeof EventType)[keyof typeof EventType];
