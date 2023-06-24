d2966af67daacf36ca35233b66b8d7c541096059
DOHEE <dohee212@naver.com>
2023-06-24 18:04:05 +0900

    [FEAT / MEME] 밈 생성 시 위경도가 함께 들어가도록 수정

--- src/meme/entity/meme.model.ts
+++ src/meme/entity/meme.model.ts
@@ -1,6 +1,7 @@
 import { BaseEntity } from 'DATABASE/entity/base.entity';
 import { IsOptional, IsString } from 'class-validator';
 import { Column, Entity, ObjectId, ObjectIdColumn } from 'typeorm';
+import { LocationModel } from './location.model';
 
 @Entity('meme_model')
 export class MemeModel extends BaseEntity {
@@ -16,4 +17,7 @@ export class MemeModel extends BaseEntity {
     @IsOptional()
     @Column({ nullable: false, comment: '메세지' })
     message: string = '';
+
+    @Column(() => LocationModel)
+    location: LocationModel;
 }
