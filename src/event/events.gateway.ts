35c632df66284c54b379c5d070ca729509a55d50
DOHEE <dohee212@naver.com>
2023-06-24 22:05:43 +0900

    [FEAT / SOCKET] 소켓 기본 기능 추가

---
+++ src/event/events.gateway.ts
@@ -0,0 +1,36 @@
+import { Logger } from '@nestjs/common';
+import {
+    ConnectedSocket,
+    MessageBody,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+    OnGatewayInit,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+} from '@nestjs/websockets';
+import { Namespace, Socket, Server } from 'socket.io';
+
+@WebSocketGateway(3000, { transports: ['websocket'], cors: true })
+export class EventsGateway {
+    private logger = new Logger('Gateway');
+    private socketMap = new Map();
+    @WebSocketServer()
+    server: Server;
+
+    handleConnection(@ConnectedSocket() socket: Socket) {
+        this.socketMap.set(socket.id, 1);
+        this.logger.log(`${socket.id} 소켓 연결`);
+    }
+
+    handleDisconnect(@ConnectedSocket() socket: Socket) {
+        this.socketMap.delete(socket.id);
+        this.logger.log(`${socket.id} 소켓 연결 해제 ❌`);
+    }
+
+    @SubscribeMessage('sendGPS')
+    async handleMessage(@ConnectedSocket() socket: Socket, @MessageBody() gps: { lat: number; long: number }) {
+        this.socketMap.set(socket.id, [gps.long, gps.lat]);
+        this.server.to(socket.id + 1).emit('sendMemes', this.socketMap.get(socket.id));
+    }
+}
