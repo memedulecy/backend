a192b5d52bd00a4b4e8669ee6195e81cf9c5f978
DOHEE <dohee212@naver.com>
2023-06-25 00:39:13 +0900

    [FEAT / SOCKET] 위치 변경 시 밈 목록 푸쉬

--- src/event/events.gateway.ts
+++ src/event/events.gateway.ts
@@ -5,6 +5,7 @@ import { EventType } from './datatypes/type/event.type';
 import { ObjectId } from 'mongodb';
 import { GpsBody, GpsData } from './datatypes/interface/gps.interface';
 import { MemeService } from 'MEME/meme.service';
+import getDistance from 'gps-distance';
 
 @WebSocketGateway(3000, { transports: ['websocket'], cors: true })
 export class EventsGateway {
@@ -27,16 +28,27 @@ export class EventsGateway {
     @SubscribeMessage(EventType.SEND_GPS)
     async handleMessage(@ConnectedSocket() socket: Socket, @MessageBody() gps: GpsBody) {
         const data: GpsData = { location: [gps.long, gps.lat] };
-        if (gps.token) data.userId = new ObjectId('649673d0a81c659607d29cd1');
+        if (gps.token) data.userId = '649673d0a81c659607d29cd1';
         this.socketMap.set(socket.id, data);
-        await this.resendMemes(30);
-        this.server.to(socket.id).emit(EventType.SEND_MEMES, this.socketMap.get(socket.id));
+        await this.resendMemes();
     }
 
-    public resendMemes = async (distance: number) => {
+    public resendMemes = async () => {
         const VALUE_IDX = 1;
-        const userIds = Array.from(this.socketMap)
-            .filter(([key, { location, userId }]) => userId && distance === 30)
-            .map(v => v[VALUE_IDX].userId);
+        await Promise.all(
+            Array.from(this.socketMap).map(async targetData => {
+                const socketId = targetData[0];
+                const [long1, lat1] = targetData[VALUE_IDX].location;
+                const userIds = Array.from(this.socketMap)
+                    .filter(socketData => {
+                        if (!socketData[VALUE_IDX].userId || targetData[VALUE_IDX].userId === socketData[VALUE_IDX].userId) return false;
+                        const [long2, lat2] = socketData[VALUE_IDX].location;
+                        return getDistance(lat1, long1, lat2, long2) <= 1;
+                    })
+                    .map(v => v[VALUE_IDX].userId);
+                const memes = await this.memeService.findByUserIds(userIds);
+                this.server.to(socketId).emit(EventType.SEND_MEMES, memes);
+            }),
+        );
     };
 }
--- src/meme/meme.service.ts
+++ src/meme/meme.service.ts
@@ -4,6 +4,8 @@ import { MemeModel } from './entity/meme.model';
 import { IntegrateException } from 'EXCEPTION/integrateException';
 import { ErrCode } from 'EXCEPTION/errCode';
 import { ErrMsg } from 'EXCEPTION/errMsg';
+import { FindOptions, In, MoreThan } from 'typeorm';
+import { before30minutes } from 'COMMON/const/time.const';
 
 @Injectable()
 export class MemeService {
@@ -17,7 +19,11 @@ export class MemeService {
     public findDetail = async (memeId: string): Promise<MemeModel> => {
         const meme = await this.memeRepository.findOneById(memeId);
         if (!meme) throw new IntegrateException(ErrCode.NOT_FOUND_MEME, ErrMsg.NOT_FOUND_MEME, HttpStatus.NOT_FOUND);
-
         return meme;
+    };
+
+    public findByUserIds = async (userIds: string[]): Promise<MemeModel[]> => {
+        const filter = { creator: { $in: userIds }, createdTs: { $gt: before30minutes } } as FindOptions<MemeModel>;
+        return await this.memeRepository.findByFilter(filter);
     };
 }
