a45e210dc4e49666f1a8b0d4c12e91051a27389a
DOHEE <dohee212@naver.com>
2023-06-24 14:04:15 +0900

    [FEAT / MEME] 밈 상세 조회 기능 추가

--- src/meme/repository/meme.repository.ts
+++ src/meme/repository/meme.repository.ts
@@ -1,5 +1,6 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { MemeModel } from 'MEME/entity/meme.model';
+import { ObjectId } from 'mongodb';
 import { MongoRepository } from 'typeorm';
 
 export class MemeRepository {
@@ -8,5 +9,9 @@ export class MemeRepository {
     public create = async (newMeme: Partial<MemeModel>): Promise<MemeModel> => {
         const meme = this.memeRepository.create(newMeme);
         return await this.memeRepository.save(meme);
+    };
+
+    public findOneById = async (memeId: string): Promise<MemeModel> => {
+        return await this.memeRepository.findOne({ where: { _id: new ObjectId(memeId) } });
     };
 }
