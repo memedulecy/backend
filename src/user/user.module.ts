b2f82f801a7523eb176025abab9b02abac6a2173
DOHEE <dohee212@naver.com>
2023-06-25 13:57:55 +0900

    [FEAT / USER] 프로필 변경 기능 완료 및 테스트

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,9 +6,17 @@ import { UserModel } from './entity/user.model';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MemeModule } from 'MEME/meme.module';
 import { EnvModule } from 'ENV/env.module';
+import { multerOptions } from 'COMMON/utils/multer.options.util';
+import { EnvService } from 'ENV/env.service';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserModel]), EnvModule, MemeModule],
+    imports: [
+        TypeOrmModule.forFeature([UserModel]),
+        MulterModule.registerAsync({ imports: [EnvModule], useFactory: multerOptions, inject: [EnvService] }),
+        EnvModule,
+        MemeModule,
+    ],
     providers: [UserService, UserRepository],
     controllers: [UserController],
     exports: [UserRepository],
