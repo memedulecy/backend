509f6053b6992767b8a16bdf0cfd522a8804f234
DOHEE <dohee212@naver.com>
2023-06-25 12:35:22 +0900

    [FEAT / USER] 소셜 로그인 기본 구조 확립

--- src/database/const/entities.const.ts
+++ src/database/const/entities.const.ts
@@ -1,3 +1,4 @@
 import { MemeModel } from 'MEME/entity/meme.model';
+import { UserModel } from 'SRC/user/entity/user.model';
 
-export const entities = [MemeModel];
+export const entities = [MemeModel, UserModel];
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserRepository } from './repository/user.repository';
+import { UserModel } from './entity/user.model';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([UserModel])],
+    providers: [UserService, UserRepository],
     controllers: [UserController],
-    providers: [UserService],
 })
 export class UserModule {}
